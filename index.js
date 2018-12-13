f09cb19a89a5fa610fd9a6ae467a9ecac0489ab3
Emile Nijssen <emile@athom.nl>
2018-12-13 17:30:45 +0100

    add migration support

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ module.exports.OAuth2Driver = require('./lib/OAuth2Driver');
 module.exports.OAuth2Device = require('./lib/OAuth2Device');
 module.exports.OAuth2Client = require('./lib/OAuth2Client');
 module.exports.OAuth2Error = require('./lib/OAuth2Error');
+module.exports.OAuth2Token = require('./lib/OAuth2Token');
+module.exports.OAuth2Util = require('./lib/OAuth2Util');
